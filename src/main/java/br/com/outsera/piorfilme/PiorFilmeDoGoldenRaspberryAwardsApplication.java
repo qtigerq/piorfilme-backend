6ffc7f098acb13e06814e581e72afc1446cb0832
Tiago Melo <chicobg@gmail.com>
2025-10-04 09:10:40 -0300

    Projeto inicial com Spring Initializr

---
+++ src/main/java/br/com/outsera/piorfilme/PiorFilmeDoGoldenRaspberryAwardsApplication.java
@@ -0,0 +1,13 @@
+package br.com.outsera.piorfilme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiorFilmeDoGoldenRaspberryAwardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiorFilmeDoGoldenRaspberryAwardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/outsera/piorfilme/PiorFilmeDoGoldenRaspberryAwardsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.outsera.piorfilme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiorFilmeDoGoldenRaspberryAwardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
